5dc53de0ba041ff888825ac6e25c03cc54e1ace1
qwabe111 <166179489+qwabe111@users.noreply.github.com>
2024-12-12 21:31:17 +0200

    Update HomeScreen.tsx

--- HomeScreen.tsx
+++ HomeScreen.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import { View, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
-const HomeScreen = () => {
-  const handlePress = () => {
-    console.log('Button pressed');
-  };
-
+const HomeScreen = ({ navigation }: any) => {
   return (
     <View style={styles.container}>
-      
+      <Text style={styles.header}>Welcome to  Private Chef Chris app</Text>
+      <Button
+        title="Go to Manage Menu"
+        onPress={() => navigation.navigate('ManageMenu')}
+      />
+      <Button
+        title="Go to Filter Menu"
+        onPress={() => navigation.navigate('FilterMenu')}
+      />
     </View>
   );
 };
@@ -18,6 +22,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#f8f8ff',
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
   },
 });
 
